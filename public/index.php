8349ce2d08572aab7307d6cdcdc046276ccecbc7
Dellon <jy.dellon@gmail.com>
2024-12-30 00:58:09 +0100

    maj

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
