e2f1608c05b24048b4bbea3b433bdb6765fb8db1
Dellon <jy.dellon@gmail.com>
2024-12-30 02:50:42 +0100

    maj

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -6,4 +6,5 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 if (file_exists(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    
 }
