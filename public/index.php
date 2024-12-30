6a32341d316824e8fb6d385d3facd3bfae3ab391
Dellon <jy.dellon@gmail.com>
2024-12-30 01:23:18 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,15 +1,9 @@
 <?php
 
+require 'bootstrap.php';
+
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-// Charge les variables d'environnement
-if (file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
