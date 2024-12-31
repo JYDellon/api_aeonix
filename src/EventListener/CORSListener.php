d24f863ec270249ff3deae90d5494935fee1b8de
Dellon <jy.dellon@gmail.com>
2024-12-31 20:23:39 +0100

    maj

--- src/EventListener/CORSListener.php
+++ src/EventListener/CORSListener.php
@@ -34,14 +34,59 @@
 
 
 
+// namespace App\EventListener;
+
+
+
+// use Symfony\Component\HttpKernel\Event\RequestEvent;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+
+// use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+// class CORSListener
+// {
+//     public function onKernelRequest(RequestEvent $event)
+//     {
+//         $request = $event->getRequest();
+
+//         // Réponse pour les requêtes OPTIONS
+//         if ($request->getMethod() === 'OPTIONS') {
+//             $response = new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, [
+//                 'Access-Control-Allow-Origin' => $request->headers->get('Origin', '*'),
+//                 'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+//                 'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+//                 'Access-Control-Allow-Credentials' => 'true',
+//             ]);
+//             $event->setResponse($response);
+//         }
+//     }
+
+//     public function onKernelResponse(ResponseEvent $event)
+//     {
+//         $response = $event->getResponse();
+//         $request = $event->getRequest();
+
+//         // Ajouter les en-têtes CORS à toutes les réponses
+//         $response->headers->set('Access-Control-Allow-Origin', $request->headers->get('Origin', '*'));
+//         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//         $response->headers->set('Access-Control-Allow-Credentials', 'true');
+//     }
+// }namespace App\EventListener;
+
+
+
+
+
+
+
 namespace App\EventListener;
 
 
 
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CORSListener
 {
@@ -49,7 +94,7 @@ class CORSListener
     {
         $request = $event->getRequest();
 
-        // Réponse pour les requêtes OPTIONS
+        // Gérer les requêtes OPTIONS
         if ($request->getMethod() === 'OPTIONS') {
             $response = new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, [
                 'Access-Control-Allow-Origin' => $request->headers->get('Origin', '*'),
