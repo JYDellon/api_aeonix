d0ed9576d3399aec5efe95f882c6596b21385668
Dellon <jy.dellon@gmail.com>
2024-12-31 10:44:49 +0100

    maj

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -16,15 +16,12 @@ class CorsListener
         $response = $event->getResponse();
         $request = $event->getRequest();
 
-        // Ajoutez les en-têtes uniquement pour les endpoints commençant par /api/
+        // Ajoutez les en-têtes uniquement pour les chemins commençant par /api/
         if (strpos($request->getPathInfo(), '/api/') === 0) {
-            $origin = $request->headers->get('Origin');
-            if ($origin === 'https://aeonix-lake.vercel.app') {
-                $response->headers->set('Access-Control-Allow-Origin', $origin);
-                $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-                $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-                $response->headers->set('Access-Control-Allow-Credentials', 'true');
-            }
+            $response->headers->set('Access-Control-Allow-Origin', 'https://aeonix-lake.vercel.app');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
         }
     }
 }
