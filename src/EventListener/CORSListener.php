53d368f14d63de8892c6e186419e9e28ec25dfe5
Dellon <jy.dellon@gmail.com>
2024-12-31 16:06:07 +0100

    maj

--- src/EventListener/CORSListener.php
+++ src/EventListener/CORSListener.php
@@ -6,37 +6,24 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class CORSListener
 {
-public function onKernelRequest(RequestEvent $event)
-{
-$request = $event->getRequest();
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
 
-// Gère les requêtes pré-volantes
-if ($request->getMethod() === 'OPTIONS') {
-$response = new JsonResponse(null, Response::HTTP_NO_CONTENT);
-$response->headers->set('Access-Control-Allow-Origin', $request->headers->get('Origin', '*'));
-$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-$response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-$response->headers->set('Access-Control-Allow-Credentials', 'true');
-$event->setResponse($response);
-}
-}
-
-public function onKernelResponse(ResponseEvent $event)
-{
-$response = $event->getResponse();
-$request = $event->getRequest();
-
-// Ajout des en-têtes CORS pour toutes les réponses
-$response->headers->set('Access-Control-Allow-Origin', $request->headers->get('Origin', '*'));
-$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-$response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-$response->headers->set('Access-Control-Allow-Credentials', 'true');
-}
+        // Intercepter les requêtes OPTIONS
+        if ($request->getMethod() === 'OPTIONS') {
+            $response = new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, [
+                'Access-Control-Allow-Origin' => $request->headers->get('Origin', '*'),
+                'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials' => 'true',
+            ]);
+            $event->setResponse($response);
+        }
+    }
 }
