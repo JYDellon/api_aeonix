bb7cf36430510e54a17376af3a7e24e592c7b6c2
Dellon <jy.dellon@gmail.com>
2025-01-01 18:47:09 +0100

    MAJ

--- public/index.php
+++ public/index.php
@@ -1,9 +1,32 @@
 <?php
 
+// use App\Kernel;
+
+// require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// return function (array $context) {
+//     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    
+// };
+
+
+
+
+
+
+
+
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    // Récupérer les variables d'environnement via $_ENV pour plus de robustesse
+    $env = $_ENV['APP_ENV'] ?? 'prod'; // Si APP_ENV n'est pas défini, on prend 'prod' par défaut
+    $debug = filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN); // Convertit APP_DEBUG en booléen
+
+    // Créez le Kernel Symfony avec ces variables d'environnement
+    $kernel = new Kernel($env, $debug);
+    return $kernel;
 };
