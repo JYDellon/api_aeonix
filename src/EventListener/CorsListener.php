033b4ce79cec1a19cc064baeefdf133b01d4559a
Dellon <jy.dellon@gmail.com>
2025-01-01 20:50:34 +0100

    MAJ

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -1,6 +1,53 @@
 <?php
 
 
+// namespace App\EventListener;
+
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpKernel\Event\RequestEvent;
+// use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+// class CorsListener
+// {
+//     public function onKernelRequest(RequestEvent $event): void
+//     {
+//         $request = $event->getRequest();
+
+//         if ($request->getMethod() === 'OPTIONS') {
+//             $response = new Response();
+//             $this->addCorsHeaders($response);
+//             $event->setResponse($response);
+//         }
+//     }
+
+//     public function onKernelResponse(ResponseEvent $event): void
+//     {
+//         $response = $event->getResponse();
+//         $this->addCorsHeaders($response);
+//     }
+
+//     private function addCorsHeaders(Response $response): void
+//     {
+//         $response->headers->set('Access-Control-Allow-Origin', 'https://aeonix-lake.vercel.app'); // Définir votre URL frontend ici
+//         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+//         $response->headers->set('Access-Control-Allow-Credentials', 'true');
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 namespace App\EventListener;
 
 use Symfony\Component\HttpFoundation\Response;
