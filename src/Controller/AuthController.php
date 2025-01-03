ddc2a668cb0cb88b3855363d180ea8d63a8da321
Dellon <jy.dellon@gmail.com>
2025-01-03 23:34:01 +0100

    test

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends AbstractController
         // Crée un cookie 'access_token' avec une durée de vie de 1 heure (3600 secondes)
         $response->headers->setCookie(
             new Cookie('access_token', $token, time() + 3600, '/', null, true, true)
+            
         );
         
         // Renvoie la réponse avec le cookie
