de66d2c4673eb298449f99f67993d6324da35306
Dellon <jy.dellon@gmail.com>
2025-01-14 12:44:17 +0100

    maj

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '06de25f3fdaa629a3cba982c6f43fa246b16c16a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '06de25f3fdaa629a3cba982c6f43fa246b16c16a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
