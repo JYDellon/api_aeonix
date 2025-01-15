4ba6f5708926211a92a86c928cf787229b071364
Dellon <jy.dellon@gmail.com>
2025-01-15 10:21:43 +0100

    maj

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    // #[Route('/', name: 'homepage', methods: ['GET'])]
-    // public function index(): Response
-    // {
-    //     // Redirige l'utilisateur vers la page de connexion
-    //     return $this->redirectToRoute('login');
-    // }
+    #[Route('/', name: 'homepage', methods: ['GET'])]
+    public function index(): Response
+    {
+        // Redirige l'utilisateur vers la page de connexion
+        return $this->redirectToRoute('login');
+    }
     
 
     #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
@@ -33,6 +33,28 @@ class SecurityController extends AbstractController
         ]);
     }
 
+
+    // #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
+    // public function login(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //     // Gestion de l'authentification
+    //     $error = $authenticationUtils->getLastAuthenticationError();
+    //     $lastUsername = $authenticationUtils->getLastUsername();
+    
+    //     return $this->render('security/login.html.twig', [
+    //         'last_username' => $lastUsername,
+    //         'error' => $error,
+    //     ]);
+    // }
+    
+    #[Route('/', name: 'redirect_to_login', methods: ['GET'])]
+    public function redirectToLogin(): Response
+    {
+        return $this->redirectToRoute('login');
+    }
+    
+
+    
     #[Route('/admin', name: 'admin', methods: ['GET'])]
     public function admin(): Response
     {
