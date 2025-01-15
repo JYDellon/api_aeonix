df222ec95dd9452757d1f5146d982e1b71a16978
Dellon <jy.dellon@gmail.com>
2025-01-15 19:30:57 +0100

    MAJ

--- src/Controller/SessionTestController.php
+++ src/Controller/SessionTestController.php
@@ -9,12 +9,26 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class SessionTestController extends AbstractController
 {
-    public function testSession(Request $request, SessionInterface $session): Response
+    // public function testSession(Request $request, SessionInterface $session): Response
+    // {
+    //     $counter = $session->get('counter', 0);
+    //     $counter++;
+    //     $session->set('counter', $counter);
+
+    //     return new Response("Counter value: $counter");
+    // }
+
+
+    public function testSession(Request $request): Response
     {
+        $session = $request->getSession();
         $counter = $session->get('counter', 0);
         $counter++;
         $session->set('counter', $counter);
 
-        return new Response("Counter value: $counter");
+        return new Response("Session counter value: $counter");
     }
+
+
+    
 }
